a440299f912a3db6a7c3e8b0606e9f09d8159a9d
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2023-05-13 11:40:54 -0500

    Punto 3

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -6,10 +6,9 @@ public class Test
 {
     public static void main(String[] args) {
         
-        Coordenadas c1 = new Coordenadas(2, 10);
-        Coordenadas c2 = new Coordenadas(3, 5);
-        Coordenadas c3 = new Coordenadas(4, 8);
-        Coordenadas c4 = new Coordenadas(5, 4);
+        Coordenadas c1 = new Coordenadas(0, 0);
+        Coordenadas c2 = new Coordenadas(1.3, 5.3);
+    
 
         System.out.println(c1.getX());
         System.out.println(c1.getY());
@@ -19,10 +18,10 @@ public class Test
         System.out.println(c2.getY());
         System.out.println(c2.tosString());
 
-        System.out.println(c1.equals(c2)? "Son iguales" : "Son diferentes");
+        System.out.println(c1.equals(c2)? "Los puntos son iguales" : "Los puntos son diferentes");
 
         //Método para calcular la distancia entre las coordenadas c1 y c2
-        
+        System.out.println("La distancia entre el punto ("+c1.getX()+" y el punto "+c2.toString()+ " es: "+c1.calcularDistancia(c2));
 
 
     }
