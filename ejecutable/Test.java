5ec389d2778f1dbbb8a509234aa9a9f0b1c25c02
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2023-05-13 11:48:41 -0500

    Fin punto 3

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -12,16 +12,16 @@ public class Test
 
         System.out.println(c1.getX());
         System.out.println(c1.getY());
-        System.out.println(c1.tosString());
+        System.out.println(c1.toString());
 
         System.out.println(c2.getX());
         System.out.println(c2.getY());
-        System.out.println(c2.tosString());
+        System.out.println(c2.toString());
 
         System.out.println(c1.equals(c2)? "Los puntos son iguales" : "Los puntos son diferentes");
 
         //Método para calcular la distancia entre las coordenadas c1 y c2
-        System.out.println("La distancia entre el punto ("+c1.getX()+" y el punto "+c2.toString()+ " es: "+c1.calcularDistancia(c2));
+        System.out.println("La distancia entre el punto ("+c1.toString()+" y el punto "+c2.toString()+ " es: "+c1.calcularDistancia(c2));
 
 
     }
