bb6001f06bc8d699f22d9de795fbbb55a16e0517
MiguelMurillo07 <jose2224644@correo.uis.edu.co>
2023-05-13 11:59:28 -0500

    Pantallazos ejercicio

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -10,18 +10,22 @@ public class Test
         Coordenadas c2 = new Coordenadas(1.3, 5.3);
     
 
-        System.out.println(c1.getX());
-        System.out.println(c1.getY());
+        System.out.println("Coordenada 1:\n");
+        System.out.println(" x = "+c1.getX());
+        System.out.println(" y = "+c1.getY());
         System.out.println(c1.toString());
 
-        System.out.println(c2.getX());
-        System.out.println(c2.getY());
+
+        System.out.println("\nCoordenada 2:\n");
+        System.out.println(" x = "+c2.getX());
+        System.out.println(" y = "+c2.getY());
         System.out.println(c2.toString());
 
-        System.out.println(c1.equals(c2)? "Los puntos son iguales" : "Los puntos son diferentes");
+        System.out.println(c1.equals(c2)? "\n\nLos puntos son iguales" : "Los puntos son diferentes\n\n");
 
         //Método para calcular la distancia entre las coordenadas c1 y c2
-        System.out.println("La distancia entre el punto ("+c1.toString()+" y el punto "+c2.toString()+ " es: "+c1.calcularDistancia(c2));
+        System.out.println("La distancia entre el punto SsS"+c1.toString()+" y el punto "+c2.toString()+ " es: "+c1.calcularDistancia(c2));
+
 
 
     }
